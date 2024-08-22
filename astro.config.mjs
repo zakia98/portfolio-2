2430c31a3b5605eb62b2d3db32dece9f8e59f4ef
Ameen <ameenahmed1998@gmail.com>
2024-08-22 19:36:49 +0100

    added github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,8 @@ import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.me',
+	site: 'https://zakia98.github.io',
+	base:'portfolio-2',
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		tailwind({
