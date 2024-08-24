c4b79721ead03f3d99b396219559ee38cd1a5dfd
Ameen <ameenahmed1998@gmail.com>
2024-08-24 14:49:06 +0100

    removed og image

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import type { AstroExpressiveCodeOptions } from 'astro-expressive-code'
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: 'SRLEOM',
+	author: 'AMEEN',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
 	title: 'Ameen Ahmed Portfolio',
 	// Meta property used as the default description meta property
